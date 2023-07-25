3a969d3ec82d30b9e4b6b0b342fc4dd07b0e16aa
nonsodaniel <nonsodaniel07@gmail.com>
2023-07-25 17:27:45 +0100

    Configured styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { CssBaseline } from "@mui/material";
+import { Fragment } from "react";
+import Grid from "@mui/material/Grid";
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+
 function App() {
-  return <div className="">Helo world!</div>;
+  return (
+    <Fragment>
+      <CssBaseline>
+        <Header />
+        <Grid container spacing={3} style={{ width: "100%" }}>
+          <Grid item xs={12} md={4}>
+            <List />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Map />
+          </Grid>
+        </Grid>
+      </CssBaseline>
+    </Fragment>
+  );
 }
 
 export default App;
