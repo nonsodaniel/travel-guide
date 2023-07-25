99cdd7524579aac3d5ab17ca08340b5711f7c0c7
nonsodaniel <nonsodaniel07@gmail.com>
2023-07-25 19:17:22 +0100

    Fixed makeStyles issues and added map

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { AppBar, Toolbar, Typography, Box, InputBase } from "@mui/material";
+// import { Autocomplete } from "@react-google-maps/api";
+import { Search } from "@mui/icons-material";
+import useStyles from "./styles";
 
 const Header = () => {
-  return <div>Header</div>;
+  const classes = useStyles();
+  return (
+    <AppBar position={"static"}>
+      <Toolbar className={classes.toolbar}>
+        <Typography className={classes.title} variant="h5">
+          Travel Guide
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h6">Explore the globe</Typography>
+          {/* <Autocomplete> */}
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <Search />
+            </div>
+            <InputBase
+              placeholder="Search..."
+              classes={{ root: classes.inputRoot, input: classes.inputInput }}
+            />
+          </div>
+          {/* </Autocomplete> */}
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Header;
--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import GoogleMapReact from "google-map-react";
+import useStyles from "./styles";
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 const Map = () => {
-  return <div>Map</div>;
+  const classes = useStyles();
+  const isMobile = useMediaQuery(`(min-width:600px)`);
+  const coordinate = { lat: 0, lan: 0 };
+  const defaultProps = {
+    center: {
+      lat: 10.99835602,
+      lng: 77.01502627,
+    },
+    zoom: 14,
+  };
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: null }}
+        center={defaultProps.center}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={() => null}
+        onChildCick={() => null}
+      ></GoogleMapReact>
+    </div>
+  );
 };
 
 export default Map;
