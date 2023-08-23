bbab3671246a0b39b6b25fad255eeb28021776cb
nonsodaniel <nonsodaniel07@gmail.com>
2023-08-23 16:04:09 +0100

    configure api access

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,5 @@
-export const API_KEY = "AIzaSyDaaeEXOe2U9WUBNCzL5PqfrcE4-eoVS1";
-export const RAPID_API_KEY =
-  "dff7849feemshaa1880c9aa2f587p18826djsn06a21ec3c30";
+export const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
+export const RAPID_API_KEY = process.env.REACT_APP_RAPID_API_KEY;
 export const noImageUrl = `https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg`;
+
+console.log({ API_KEY, RAPID_API_KEY });
